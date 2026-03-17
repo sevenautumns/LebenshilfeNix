9e138c3c122b42c2adafef312ed3e9dd124eb59d
Autumnal <github@p.autumnal.de>
2026-03-17 20:50:24 +0100

    feat: add EditModeMixin with read-only/edit mode toggle to admin

--- pkgs/lebenshilfe-cms/base/admin.py
+++ pkgs/lebenshilfe-cms/base/admin.py
@@ -1,5 +1,11 @@
+from typing import Union
 from django.contrib import admin
+from django.http import HttpRequest 
+from django.shortcuts import redirect
+from django.urls import reverse
 from unfold.admin import ModelAdmin, GenericTabularInline
+from unfold.decorators import action
+from unfold.enums import ActionVariant
 from .models import (
     Person, Address, Phone, Email, BankAccount, 
     CostPayerLink, MasterData, ExternalIdentifier, 
@@ -13,7 +19,38 @@ admin.site.unregister(SocialAccount)
 admin.site.unregister(SocialToken)
 admin.site.unregister(EmailAddress)
 
-class BaseModelAdmin(ModelAdmin):
+class EditModeMixin:
+    actions_detail = ["edit_action"]
+
+    def _get_change_url(self, object_id: int) -> str:
+        return reverse(
+            f"admin:{self.opts.app_label}_{self.opts.model_name}_change", 
+            args=[object_id]
+        )
+
+    @action(
+        description="Bearbeiten",
+        url_path="edit-action",
+        permissions=["edit_action"],
+        variant=ActionVariant.PRIMARY,
+    )
+    def edit_action(self, request: HttpRequest, object_id: int):
+        url = self._get_change_url(object_id)
+        return redirect(f"{url}?edit=1")
+
+    def has_edit_action_permission(self, request, obj=None):
+        return not self.is_edit(request)
+
+    def is_edit(self, request):
+        return request.GET.get('edit') == '1'
+
+    def has_change_permission(self, request, obj=None):
+        has_class_permission = super().has_change_permission(request, obj)
+        if not has_class_permission:
+            return False
+        return self.is_edit(request)
+    
+class BaseModelAdmin(EditModeMixin, ModelAdmin):
     hour_minute_fields = []
     currency_fields = []
 
